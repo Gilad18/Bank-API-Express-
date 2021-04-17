d9176aa3bfbeddc7e8a1058872660b7e1d62320f
Gilad <gilad18587@gmail.com>
2021-04-17 22:10:48 +0300

    final

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,6 @@
 const dataJSON = require('../accounts.json')
 const fs = require('fs');
 
-const getAccounts = () => {
-    return dataJSON.accounts
-}
 
 const getAccountByPassport = (req) => {
     return dataJSON.accounts.filter(item => item.passport === req)
@@ -17,21 +14,20 @@ const getClientsByAmount = (req,res) => {
 }
 
 const addNewAccount = (req, res) => {
-    const newAccount = {
-        passport: req.params.passport,
-        cash: 0,
-        credit: 0
-    }
-    let tempJSON = dataJSON
-    tempJSON.accounts.push(newAccount)
-    fs.writeFileSync('./accounts.json', JSON.stringify(tempJSON))
-    return res.status(200).json({ success: 'new account was successfully created' })
+    let checkIfExsit = getAccountByPassport(req.params.passport)
+        if(!checkIfExsit[0]) {
+            let tempJSON = dataJSON
+            tempJSON.accounts.push({passport: req.params.passport,cash: 0,credit: 0})
+            fs.writeFileSync('./accounts.json', JSON.stringify(tempJSON))
+            return res.status(200).json({ success: 'new account was successfully created' })
+        }
+     else {res.status(200).json({ error: 'Client already exist' })}
 }
 
 const deposit = (req, res) => {
-    let selected = getAccountByPassport(req.params.passport);
+        let selected = getAccountByPassport(req.params.passport);
     if (selected[0]) {
-        selected.map(item => item.cash = item.cash + parseInt(req.params.amount));
+        selected.map(item => item.cash += parseInt(req.params.amount));
         fs.writeFileSync('./accounts.json', JSON.stringify(dataJSON));
         return res.status(200).json({ success: 'Deposit was succesfully made' })
     }
@@ -52,7 +48,7 @@ const withdrawl = (req, res) => {
     let selected = getAccountByPassport(req.params.passport);
     if (selected[0]) {
         if (req.params.amount <= selected[0].cash + selected[0].credit) {
-            selected.map(item => item.cash = item.cash - req.params.amount)
+            selected.map(item => item.cash -= req.params.amount)
             fs.writeFileSync('./accounts.json', JSON.stringify(dataJSON));
             return res.status(200).json({ success: 'Withdrawal was successfuly made' })
         } else { res.status(200).json({ error: 'client doesnt have sufficent funds for this action' }) }
@@ -61,12 +57,12 @@ const withdrawl = (req, res) => {
 }
 
 const transfer = (req,res) => {
-    let sender = getAccountByPassport(req.params.passport)
+    let sender = getAccountByPassport(req.params.passport1)
     let reciever = getAccountByPassport(req.params.passport2)
     if (sender[0] && reciever[0]) {
         if (req.params.amount <= sender[0].cash + sender[0].credit) {
-            sender.map(item => item.cash = item.cash - parseInt(req.params.amount))
-            reciever.map(item => item.cash = item.cash + parseInt(req.params.amount))
+            sender.map(item => item.cash -= parseInt(req.params.amount))
+            reciever.map(item => item.cash += parseInt(req.params.amount))
             fs.writeFileSync('./accounts.json', JSON.stringify(dataJSON));
             return res.status(200).json({ success: 'Transfer was successfuly made' })
         } else { res.status(200).json({ error: 'client doesnt have sufficent funds for this action' }) }
@@ -75,7 +71,6 @@ const transfer = (req,res) => {
 }
 
 module.exports = {
-    getAccounts,
     getAccountByPassport,
     addNewAccount,
     deposit,
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const dataJSON = require('../accounts.json');
 const controllers = require('../controllers/controllers')
 
@@ -9,8 +8,10 @@ router.get('/', (req,res) => {
 }).get('/accounts/:passport' , (req , res) => {
     return res.send(controllers.getAccountByPassport(req))
 }).post('/:passport' , (req,res) => {
-    controllers.addNewAccount(req,res);
-}).put('/accounts/:passport/cash/:amount' , (req,res) => {
+    if(req.params.passport.length>=6) {
+        controllers.addNewAccount(req,res);
+    } else {res.status(200).json({ error: 'Invalid Passport, must include min 6 chars' })}
+}).put('/accounts/:passport/deposit/:amount' , (req,res) => {
     if(req.params.amount>0) {
         controllers.deposit(req,res);
     } else {res.status(200).json({ error: 'Despoit must be with a positive value' })} 
@@ -22,7 +23,7 @@ router.get('/', (req,res) => {
     if(req.params.amount>0) {
         controllers.withdrawl(req,res)
     } else {res.status(200).json({ error: 'Withdrawl must be with a positive value' })} 
-}).put('/accounts/:passport/transfer/:amount/:passport2',(req,res) => {
+}).put('/accounts/:passport1/transfer/:amount/:passport2',(req,res) => {
     if(req.params.amount>0){
         controllers.transfer(req,res)
     }else {res.status(200).json({ error: 'Transfer must be with a positive value' })} 
