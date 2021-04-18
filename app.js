d21cebdf79d74cc8c90f81c58f3c197db0a17239
Gilad <gilad18587@gmail.com>
2021-04-18 15:42:06 +0300

    hereku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
-const port =1805;
+const port =5000;
 const accountRoute = require('./routes/accountRoute');
 
 
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,6 +14,11 @@ app.use(bodyParser.json());
 app.use('/api',accountRoute);
 
 
-app.listen(port,()=>{
-    console.log(`application start at ${port}`)
+// app.listen(port,()=>{
+//     console.log(`application start at ${port}`)
+// })
+
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`application start at ${port}`);
 })
